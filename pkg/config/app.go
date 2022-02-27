1af33337f03313476f94a4b5d13e3dd87cb53dcc
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2022-02-26 20:53:37 -0800

    completed project

---
+++ pkg/config/app.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	// "github.com/jinzhu/gorm"
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	// d, err := gorm.Open("mysql", "root:<password>/<dbname>?charset=utf8&parseTime=True&loc=Local")
+	dsn := "root:<password>@tcp(127.0.0.1:3306)/golang_bookmanagement_db?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
